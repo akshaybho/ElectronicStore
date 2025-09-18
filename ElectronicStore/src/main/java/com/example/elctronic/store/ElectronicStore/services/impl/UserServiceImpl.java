343c04d8d1ee998dacaf9342d1e4021e93165757
akshaybho <bhogale.akshay11@gmail.com>
2025-09-18 12:08:38 +0530

    user image delete

--- ElectronicStore/src/main/java/com/example/elctronic/store/ElectronicStore/services/impl/UserServiceImpl.java
+++ ElectronicStore/src/main/java/com/example/elctronic/store/ElectronicStore/services/impl/UserServiceImpl.java
@@ -8,7 +8,10 @@ import com.example.elctronic.store.ElectronicStore.helper.Helper;
 import com.example.elctronic.store.ElectronicStore.repositories.UserRepository;
 import com.example.elctronic.store.ElectronicStore.services.UserService;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -16,6 +19,11 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -29,6 +37,11 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private ModelMapper mapper;
+
+    @Value("${user.profile.image.path}")
+    private String imagePath;
+
+    private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Override
     public UserDto createUser(UserDto userDto) {
@@ -66,6 +79,23 @@ public class UserServiceImpl implements UserService {
     public void deleteUser(String userId) {
 
         User user = userRepository.findById(userId).orElseThrow(()-> new ResourceNotFoundException("User not found !!"));
+
+        String fullPath = imagePath + user.getImageName();
+
+        try{
+            Path path = Paths.get(fullPath);
+            Files.delete(path);
+        }
+        catch(FileNotFoundException ex)
+        {
+            logger.info("User image not found in folder");
+            ex.printStackTrace();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+
         userRepository.delete(user);
     }
 
